b2a8884979f6c6da727fd68703c3ff484553c4cb
Victor Gauthereau <vgauther@air-de-victor.home>
2022-10-19 13:55:09 +0200

    clean

--- OrangeTangerineKNN.py
+++ OrangeTangerineKNN.py
@@ -8,52 +8,55 @@ Created on Sun Oct 16 18:13:33 2022
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd 
+import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from KNN import KNeighbors
 
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
 
-# for i in range (len(train.label)):
-#     if train.label[i] == 0:
-#         train.label[i] = 'Orange'
-#     else: 
-#         train.label[i] = 'Tangerine'
+def main():
+    train = pd.read_csv("train.csv")
+    test = pd.read_csv("test.csv")
 
+    # for i in range (len(train.label)):
+    #     if train.label[i] == 0:
+    #         train.label[i] = 'Orange'
+    #     else:
+    #         train.label[i] = 'Tangerine'
 
-train = np.array(train[['Fruit Size','Fruit Color','Leaf Size','label']])   
-xtrain_norm = np.zeros((200,2))
-xtest=np.array(test[['Fruit Size','Fruit Color','Leaf Size']])
-
-    
-# orange = np.zeros((200,2))        
-# tangerine = np.zeros((200,2))  
-# for i in range (len(train.label)):
-#     if train.label[i] == 'Orange':
-#         orange[i,0] = xtrain_norm[i, 0]
-#         orange[i,1] = xtrain_norm[i, 1]
-#     else: 
-#         tangerine[i,0] = xtrain_norm[i, 0]
-#         tangerine[i,1] = xtrain_norm[i, 1]
-        
-# plt.scatter(tangerine[:,0], tangerine[:,1], color = 'r')
-# plt.scatter(orange[:,0], orange[:,1], color = 'b')
-# plt.show()
+    train = np.array(train[['Fruit Size','Fruit Color','Leaf Size','label']])
+    # xtrain_norm = np.zeros((200,2))
+    xtest=np.array(test[['Fruit Size','Fruit Color','Leaf Size']])
 
 
-kn=KNeighbors(3, train, xtest)
-neighbors= kn.predict(xtest[0])
+    # orange = np.zeros((200,2))
+    # tangerine = np.zeros((200,2))
+    # for i in range (len(train.label)):
+    #     if train.label[i] == 'Orange':
+    #         orange[i,0] = xtrain_norm[i, 0]
+    #         orange[i,1] = xtrain_norm[i, 1]
+    #     else:
+    #         tangerine[i,0] = xtrain_norm[i, 0]
+    #         tangerine[i,1] = xtrain_norm[i, 1]
 
-predict=list()
-for x in xtest:
-    pred = kn.predict(x)
-    predict.append(pred)
-predict = np.array(predict)
-prediction = []
-for i in range (len(predict)):
-    if predict[i] == 0:
-        prediction.append('Orange')
-    else: 
-        prediction.append('Tangerine')
+    # plt.scatter(tangerine[:,0], tangerine[:,1], color = 'r')
+    # plt.scatter(orange[:,0], orange[:,1], color = 'b')
+    # plt.show()
 
+
+    kn = KNeighbors(3, train, xtest)
+    neighbors = kn.predict(xtest[0])
+
+    predict = list()
+    for x in xtest:
+        pred = kn.predict(x)
+        predict.append(pred)
+    predict = np.array(predict)
+    prediction = []
+    for i in range (len(predict)):
+        if predict[i] == 0:
+            prediction.append('Orange')
+        else:
+            prediction.append('Tangerine')
+
+if __name__ == "__main__":
+    main()
